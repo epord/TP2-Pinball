1efa79174bfd11ae056d0979f193507b46e12ea2
HubertHamelin <32563790+HubertHamelin@users.noreply.github.com>
2018-10-02 21:07:14 -0300

    GameManager and highScore

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static int _highScore;
+    private const string HIGHSCORE = "highScore";
+    private ScoreManager scoreManager;
+
+	void Start ()
+    {
+        _highScore = PlayerPrefs.GetInt(HIGHSCORE, _highScore);
+        scoreManager = ScoreManager.GetInstance();
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnDestroy()
+    {
+        int currentScore = (int)scoreManager.GetScore();
+        if ( currentScore > _highScore)
+        {
+            PlayerPrefs.SetInt(HIGHSCORE, currentScore);
+        }
+    }
+}
